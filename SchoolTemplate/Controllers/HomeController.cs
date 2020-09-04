d80ad012504d5b2cc93923eab3472b8a2274a905
110074@st-maartenscollege.nl <110074@st-maartenscollege.nl>
2020-09-04 14:47:46 +0200

    database

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace SchoolTemplate.Controllers
   public class HomeController : Controller
   {
     // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=LifeUniverseEverything;";
+    string connectionString = "Server=172.16.160.21;Port=3306;Database=110074;Uid=110074;Pwd=uTuRgent;";
 
     public IActionResult Index()
     {
